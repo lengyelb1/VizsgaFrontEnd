d1312ba8c5da51db7311ec9e62b7cfc3be2aa619
lengyelb <lengyelb@kkszki.hu>
2024-03-12 20:52:07 +0100

    Admin Dashboard and fixes

--- src/Components/Admin/AdminHomePage.js
+++ src/Components/Admin/AdminHomePage.js
@@ -5,13 +5,32 @@ import './AdminHomePage.css';
 export default function AdminHomePage () {
 
     const [data,setData] = useState([]);
+    const [userDb,setUserDb] = useState();
+    const [postDb,setPostDb] = useState();
     
+    const [userByName,setUserByName] = useState();
+    const [postByName,setPostByName] = useState();
+
+
     useEffect(()=>{
         fetch("http://localhost:7043/AdminUsers",{headers:{'Authorization': `Bearer ${localStorage.getItem('token')}`}})
         .then((response) => response.json())
         .then((resp)=> {
             setData(resp)
         })
+
+        fetch("http://localhost:7043/AdminUsers/userdb",{headers:{'Authorization': `Bearer ${localStorage.getItem('token')}`}})
+        .then((response) => response.json())
+        .then((resp)=> {
+            setUserDb(resp)
+        })
+
+        fetch("http://localhost:7043/AdminUsers/userpostdb",{headers:{'Authorization': `Bearer ${localStorage.getItem('token')}`}})
+        .then((response) => response.json())
+        .then((resp)=> {
+            setPostDb(resp)
+        })
+        
     },[])
     
 
@@ -29,22 +48,28 @@ export default function AdminHomePage () {
                 <br/>
                 <div>
                     <div className="card-green p-2 rounded">
-                        <p>Users: {data.length}</p>
+                        <p>Users: {userDb}</p>
+                        <p>Posts: {postDb}</p>
                     </div>
                 </div>
-                <div className="">
-                    <h2 className="text-green">Users</h2>
-                    <ListUsers data ={data}/>
+                <div className="row mt-3 min-vh-100 w-100">
+                    <div className="col-6">
+                        <ListUsersByName userByName = {userByName}/>
+                    </div>
+                    <div className="col-6">
+                        <ListPostsByName postByName = {postByName}/>
+                    </div>
                 </div>
 
             </div>
         </div>
     )
-}
 
-function ListUsers(params){
-    console.log(params.data)
-    return params.data.map((user) => (
+
+function UserKi (prop) {
+    return (
+    <div>
+    {prop.resp.map((user) => (
         <div key={user.id + 1} className='card col-12 p-2 bg-dark text-light mx-auto mt-3 border border-dark shadow-green'>
             <div className='card-body'>
                 <p className='text-green'>{user.username}</p>
@@ -52,9 +77,119 @@ function ListUsers(params){
                 <div className='small'>Email: {user.email}</div>
                 <a href= {"AdminPutSingleUser/"+user.id} className="btn btn-warning">Change</a>
                 <a href={"AdminDeleteUser/"+user.id} className="btn btn-danger ms-2">Delete</a>
+            </div>    
+        </div>
+    ))}
+    </div>
+    )
+}
 
-            </div>
+function UserByNameFrom () {
+    return (
+        <form onSubmit={(e) =>{
+            e.preventDefault()
+            if (e.target.elements.searchByNameUser1.value == null || e.target.elements.searchByNameUser1.value == "") {
+                alert("Please enter something in the searchbar!")
+            }else{
+                fetch(`http://localhost:7043/AdminUsers/nev?nev=${e.target.elements.searchByNameUser1.value}`,{headers:{'Authorization': `Bearer ${localStorage.getItem('token')}`}})
+                .then((response) => response.json())
+                .then((resp)=> {
+                    setUserByName(resp)
+                })
+            }
+        }}>
+          <div className="mb-3">
+            <label htmlFor="searchByNameUser1" className="form-label text-green">User Name: </label>
+            <input type="text" className="form-control" id="searchByNameUser1" aria-describedby="searchByNameUser1"/>
+          </div>
+          <button type="submit" className="btn btn-primary">Search</button>
+        </form>
+
+    )
+}
+
+function ListUsersByName(prop){
+    if (userByName == null) {
+        return (
+            <div>
+                <UserByNameFrom/>
+                <div>
+        
+                </div>
+            </div>) 
+    }
+    else{
+        return (
+            <div>
+                <UserByNameFrom/>
+                <div>
+                    <UserKi resp = {prop.userByName} />
+                </div>
+            </div>) 
+    }
+}
+
+function PostsByNameForm () {
+    return(
+        <form onSubmit={(e) =>{
+            e.preventDefault()
+            if (e.target.elements.searchByNamePost1.value == null || e.target.elements.searchByNamePost1.value == "") {
+                alert("Please enter something in the searchbar!")
+            }else{
+                fetch(`http://localhost:7043/AdminUsers/postnev?nev=${e.target.elements.searchByNamePost1.value}`,{headers:{'Authorization': `Bearer ${localStorage.getItem('token')}`}})
+                .then((response) => response.json())
+                .then((resp)=> {
+
+                    setPostByName(resp)
+
+                })
+            }
             
+        }}>
+          <div className="mb-3">
+            <label htmlFor="searchByNamePost1" className="form-label text-green">Post Name: </label>
+            <input type="text" className="form-control" id="searchByNamePost1" aria-describedby="searchByNamePost1"/>
+          </div>
+          <button type="submit" className="btn btn-primary">Search</button>
+        </form>
+    )
+}
+
+
+function ListPostsByName(prop){
+
+    if (postByName == null) {
+        return (
+            <div>
+                <PostsByNameForm/>
+                <div>
+        
+                </div>
+            </div>) 
+    }
+    else{
+        return (
+            <div>
+                <PostsByNameForm/>
+                <div>
+                    <PostsKi posts = {prop.postByName} />
+                </div>
+            </div>) 
+    }
+}
+
+function PostsKi (params) {
+    return params.posts.map((post) => (
+        <div key={post.id + 1} className='card col-md-5 p-2 bg-dark text-light mx-auto mt-3 border border-dark shadow-green'>
+            <div className='card-body text-decoration-none'>
+                <h5 className='text-light'>{post.userId}</h5>
+                <h5 className=''>{post.title}</h5>
+                <div className='small'>{post.description}</div>
+            </div>
+            <div className='text-green align-middle'>
+                Likes: {post.like}
+            </div>
         </div>    
     ))
+}
 }
