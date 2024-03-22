8fe953bcc687d2a5f597988cef4218ee0572e00f
lbálint <lengyelb@kkszki.hu>
2024-03-22 10:25:53 +0100

    User name display insted of userId

--- src/Components/User/UserHomePage_V2.js
+++ src/Components/User/UserHomePage_V2.js
@@ -126,7 +126,7 @@ export default function UserHomePageV2(){
                 return params.posts.map((post) => (
                     <div key={post.id + 1} className='card col-md-5 p-2 bg-dark text-light mx-auto mt-3 border border-dark shadow-green'>
                         <a className='card-body text-decoration-none' href={`/SinglePostDisplay/${post.id}`}>
-                            <h5 className='text-light'>{post.userId}</h5>
+                            <h5 className='text-light'>{post.user.username}</h5>
                             <h5 className=''>{post.title}</h5>
                             <div className='small'>{post.description}</div>
                         </a>
