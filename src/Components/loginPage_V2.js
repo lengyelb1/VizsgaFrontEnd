1a11d18eec5f9f3da634b26765c3a5f9961e9bcb
lengyelb <lengyelb@kkszki.hu>
2024-03-29 12:01:34 +0100

    Toasts on loginPage

--- src/Components/loginPage_V2.js
+++ src/Components/loginPage_V2.js
@@ -4,10 +4,20 @@ import Logo from './Back4.png';
 import axios from 'axios';
 import { useNavigate } from "react-router-dom";
 import { jwtDecode } from 'jwt-decode';
+import Toast from 'react-bootstrap/Toast';
+import { useState } from 'react';
+import ReactDOM from 'react-dom/client';
 
 
 export default function LoginPageV2 (props) {
     const navigate = useNavigate();
+
+    const [showA, setShowA] = useState(false);
+    const [_error, setError] = useState();
+    const [inputEmpty, setInputEmpty] = useState(true);
+        
+    const toggleShowA = () => setShowA(!showA);
+
     return (
     <div className="bg-dark text-light vh-100 vw-100 mx-auto" data-bs-theme="dark">
         <div className='d-flex h-25' style={{ background: 'linear-gradient(to bottom, #00ADB5,#A8F231)',borderRadius:'0px 0px 0px 50px', overflow:'hidden'}}>
@@ -17,39 +27,45 @@ export default function LoginPageV2 (props) {
           <div className="d-flex vw-100" style={{ borderRadius:'0px 50px 0px 0px', overflow:'hidden'}}>
               <Form className='bg-dark p-2 vw-100 ' onSubmit={(e)=>{
                 e.preventDefault();
-                e.target.elements.loginButton.disabled = true
-                e.target.elements.loginButton.innerHTML = `<div className="spinner-border " role="status">
-                    <span className="visually-hidden text-green">Loading...</span>
-                </div>`
-                axios({
-                  method: 'post',
-                  url: 'http://localhost:7043/Auth/login',
-                  data: {
-                    userName: document.getElementById('username').value,
-                    password: document.getElementById('password').value
-                  }
-                })
-                .then((resp) => {
-                  if (resp.name =="AxiosError") {
-                    return (alert(resp.response.data))
-                  }else{
-                    localStorage.setItem("token",resp.data);
-
-                    if (jwtDecode(localStorage.getItem("token")).role == props.USER_TYPES.ADMIN_USER) {
-                      navigate("/AdminHomePage");                      
-                    }else{
-                      navigate("/UserHomePage");
+                if(document.getElementById('username').value == "" || document.getElementById('password').value == ""){
+                  setInputEmpty(true)
+                  toggleShowA()
+                }else{
+                  e.target.elements.loginButton.disabled = true
+                  e.target.elements.loginButton.innerHTML = `<div className="spinner-border " role="status">
+                      <span className="visually-hidden text-green">Loading...</span>
+                  </div>`
+                  axios({
+                    method: 'post',
+                    url: 'http://localhost:7043/Auth/login',
+                    data: {
+                      userName: document.getElementById('username').value,
+                      password: document.getElementById('password').value
                     }
-                  }
-                })
-                .catch((error)=> {
-                  console.log(error)
-                  
-                })
-                .finally(()=>{
-                  e.target.elements.loginButton.disabled = false
-                  e.target.elements.loginButton.innerHTML = `Submit`
-                })
+                  })
+                  .then((resp) => {
+                    if (resp.name =="AxiosError") {
+                      return (alert(resp.response.data))
+                    }else{
+                      localStorage.setItem("token",resp.data);
+  
+                      if (jwtDecode(localStorage.getItem("token")).role == props.USER_TYPES.ADMIN_USER) {
+                        navigate("/AdminHomePage");                      
+                      }else{
+                        navigate("/UserHomePage");
+                      }
+                    }
+                  })
+                  .catch((error)=> {
+                    console.log(error)
+                    setError(error)
+                    toggleShowA()
+                  })
+                  .finally(()=>{
+                    e.target.elements.loginButton.disabled = false
+                    e.target.elements.loginButton.innerHTML = `Submit`
+                  })
+                }
               }}>
                 <Form.Label className='h1'>Login</Form.Label>
                 <Form.Group className="mb-3 text-light" controlId="formBasic">
@@ -73,8 +89,61 @@ export default function LoginPageV2 (props) {
                 </Button>
               </Form>
           </div>
+          
         </div>
-        
+        <div className='float-end'>
+          <MyToast header = "Alert!" error={_error}></MyToast>
+        </div>
     </div>
     )
+
+    function MyToast(params) {
+      if (params.error) {
+        return (
+          <Toast show={showA} onClose={toggleShowA} className='bg-danger'>
+          <Toast.Header>
+            <img
+              src="holder.js/20x20?text=%20"
+              className="rounded me-2"
+              alt=""
+            />
+            <strong className="me-auto">{params.header}</strong>
+          </Toast.Header>
+          <Toast.Body>{params.error.response.data}</Toast.Body>
+        </Toast>
+        );
+      }else if (inputEmpty){
+        return (
+          <Toast show={showA} onClose={toggleShowA} className='bg-warning'>
+          <Toast.Header>
+            <img
+              src="holder.js/20x20?text=%20"
+              className="rounded me-2"
+              alt=""
+            />
+            <strong className="me-auto">{params.header}</strong>
+          </Toast.Header>
+          <Toast.Body>Please enter the username and password</Toast.Body>
+        </Toast>
+        );
+      }else{
+        return (
+          <Toast show={showA} onClose={toggleShowA} className='bg-danger'>
+          <Toast.Header>
+            <img
+              src="holder.js/20x20?text=%20"
+              className="rounded me-2"
+              alt=""
+            />
+            <strong className="me-auto">{params.header}</strong>
+          </Toast.Header>
+          <Toast.Body>Unknown</Toast.Body>
+        </Toast>
+        );
+      }
+      
+      
+    }
+    
 }
+
